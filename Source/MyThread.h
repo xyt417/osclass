2a8413153ecbe1faedcce72213e7540a9e7c57d5
xyt417 <417639959@qq.com>
2023-05-27 18:36:29 +0800

    增加设备防止饿死

--- Source/MyThread.h
+++ Source/MyThread.h
@@ -16,39 +16,50 @@ public:
     }
     void run() override {
       // 测试设备处理线程
-        int n = 100, m = 100;
-        while(n --){
-            deviceQueue.allocate_device("printer", "p1", "print,p1: hello printer num:" + to_string(m - n), 1);
-            deviceQueue.allocate_device("printer", "p2", "print,p2: hello printer num:" + to_string(m - n), 2);
-            deviceQueue.allocate_device("printer", "p3", "print,p3: hello printer num:" + to_string(m - n));
-            deviceQueue.allocate_device("printer", "p0", "print,p0: hello printer num:" + to_string(m - n));
-            deviceQueue.allocate_device("printer", "p0", "print,p0: hello printer num:" + to_string(m - n));
-        }
-        // 暂停 3 秒 等待设备处理线程处理任务
-        QThread::msleep(3000);
-        emit stopSignal(); // 测试暂停设备处理线程
-        n = 30, m = 30;
-        while(n --){
-            deviceQueue.allocate_device("screen", "p1", "print,p1: hello screen num:" + to_string(m - n));
-            deviceQueue.allocate_device("screen", "p2", "print,p2: hello screen num:" + to_string(m - n));
-            deviceQueue.allocate_device("screen", "p3", "print,p3: hello screen num:" + to_string(m - n));
-        }
-        // 暂停 5 秒
-        QThread::msleep(5000);
-        n = 200, m = 200;
-        while(n --){
-            deviceQueue.allocate_device("screen", "p0", "print,p0: hello screen num:" + to_string(m - n));
-            deviceQueue.allocate_device("printer", "p0", "print,p0: hello screen num:" + to_string(m - n));
-        }
-        emit startSignal(); // 测试启动设备处理线程
-        // 磁盘写入测试
-        deviceQueue.allocate_device("disk", "p0", "write,0,10,hello disk0.", 3);
-        deviceQueue.allocate_device("disk", "p0", "write,1,10,hello disk1.", 3);
-        deviceQueue.allocate_device("disk", "p1", "read,0,10,30,0", 3);
-        deviceQueue.allocate_device("disk", "p1", "read,1,0,30,1", 3);
+      // Test 1
+        // int n = 100, m = 100;
+        // while(n --){
+        //     deviceQueue.allocate_device("printer", "p1", "print,p1: hello printer num:" + to_string(m - n), 1);
+        //     deviceQueue.allocate_device("printer", "p2", "print,p2: hello printer num:" + to_string(m - n), 2);
+        //     deviceQueue.allocate_device("printer", "p3", "print,p3: hello printer num:" + to_string(m - n));
+        //     deviceQueue.allocate_device("printer", "p0", "print,p0: hello printer num:" + to_string(m - n));
+        //     deviceQueue.allocate_device("printer", "p0", "print,p0: hello printer num:" + to_string(m - n));
+        // }
+        // // 暂停 3 秒 等待设备处理线程处理任务
+        // QThread::msleep(3000);
+        // emit stopSignal(); // 测试暂停设备处理线程
+        // n = 30, m = 30;
+        // while(n --){
+        //     deviceQueue.allocate_device("screen", "p1", "print,p1: hello screen num:" + to_string(m - n));
+        //     deviceQueue.allocate_device("screen", "p2", "print,p2: hello screen num:" + to_string(m - n));
+        //     deviceQueue.allocate_device("screen", "p3", "print,p3: hello screen num:" + to_string(m - n));
+        // }
+        // // 暂停 5 秒
+        // QThread::msleep(5000);
+        // n = 200, m = 200;
+        // while(n --){
+        //     deviceQueue.allocate_device("screen", "p0", "print,p0: hello screen num:" + to_string(m - n));
+        //     deviceQueue.allocate_device("printer", "p0", "print,p0: hello screen num:" + to_string(m - n));
+        // }
+        // emit startSignal(); // 测试启动设备处理线程
+        // // 磁盘写入测试
+        // deviceQueue.allocate_device("disk", "p0", "write,0,10,hello disk0.", 3);
+        // deviceQueue.allocate_device("disk", "p0", "write,1,10,hello disk1.", 3);
+        // deviceQueue.allocate_device("disk", "p1", "read,0,10,30,0", 3);
+        // deviceQueue.allocate_device("disk", "p1", "read,1,0,30,1", 3);
 
-        QThread::msleep(10000);
-        cout << "buffer0:" << deviceQueue.readInBuffer[0] << endl;
-        cout << "buffer1:" << deviceQueue.readInBuffer[1] << endl;
+        // QThread::msleep(10000);
+        // cout << "buffer0:" << deviceQueue.readInBuffer[0] << endl;
+        // cout << "buffer1:" << deviceQueue.readInBuffer[1] << endl;
+
+        // Test 2
+        QThread::msleep(3000);
+        emit stopSignal();
+        int n = 1000, m = 1000;
+        while(n --){
+            deviceQueue.allocate_device("printer", "p1", "print,p1: hello printer num:" + to_string(m - n));
+            deviceQueue.allocate_device("screen", "p1", "print,p2: hello printer num:" + to_string(m - n));
+        }
+        emit startSignal();
     }
 };
